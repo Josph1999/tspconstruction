a8036cd25c91427a562448605bd3e400e6dbb954
Ioseb Guruli <iosebguruli@admins-MacBook-Pro.local>
2026-05-08 15:25:32 +0400

    lang selection

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -4,6 +4,7 @@ export const routing = defineRouting({
   locales: ["en", "ka"] as const,
   defaultLocale: "ka",
   localePrefix: "always",
+  localeDetection: false,
 });
 
 export type Locale = (typeof routing.locales)[number];
